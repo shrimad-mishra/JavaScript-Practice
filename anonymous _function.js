61dcaec148dc4c33e28722bee133fa0bf8584431
Shrimad Mishra <mshrimad@gamil.com>
2024-07-15 00:36:04 +0530

    initial

---
+++ anonymous _function.js
@@ -0,0 +1,18 @@
+/*
+They are the function without name
+
+function() {
+    // Function Body
+}
+
+( () => {
+    // Function Body...
+} )();
+
+*/
+
+var greet = function () { 
+    console.log("Test ..................................."); 
+}; 
+  
+greet(); 
